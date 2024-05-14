9f7f376490f5fb5ee74f327233d9e1f3bec220a3
frtewygdsuhiw <hsjcarvinflores@hotmail.com>
2024-05-14 19:17:08 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-function wiggleMaxLength(nums) {
-  if (nums.length === 0) return 0;
-  let up = 1;
-  let down = 1;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] > nums[i - 1]) {
-      up = down + 1;
-    } else if (nums[i] < nums[i - 1]) {
-      down = up + 1;
-    }
-  }
-  return Math.max(up, down);
-}
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
