40ecde13fbe1df3ba5d6a0fa794701da74738f46
frtewygdsuhiw <hsjcarvinflores@hotmail.com>
2024-05-16 06:22:42 +0700

    feat project

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-console.log(randomNumber);
+function isValidBST(root) {
+  return isValid(root, null, null);
+  function isValid(node, min, max) {
+    if (!node) return true;
+    if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+      return false;
+    return (
+      isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+    );
+  }
+}
